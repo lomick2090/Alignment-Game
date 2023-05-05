93796258f525bede4ef7431f853d86afb7428e3a
minkman <lomick2090@gmail.com>
2023-05-05 15:17:59 -0500

    Add full user addition

--- frontend/src/components/AddUserToList.jsx
+++ frontend/src/components/AddUserToList.jsx
@@ -1,19 +1,20 @@
-import { db, auth } from '../config/firebase'
+import { db, auth, storage } from '../config/firebase'
 import { getDocs, addDoc, collection } from 'firebase/firestore'
+import { getStorage, ref, uploadBytes } from "firebase/storage"
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 
 export default function AddUserToList() {
+    const [groups, setGroups] = useState([])
     const [user, setUser] = useState({
         name: '',
         img: '',
-        group:''
+        group: ''
     })
-    const [groups, setGroups] = useState([])
 
-    const groupsRef = collection(db, 'groups')
-    const usersRef = collection(db, 'users')
+    const groupsRef = collection(db, 'groups');
+    const usersRef = collection(db, 'users');
 
     useEffect(() => {
         async function getGroupList() {
@@ -25,45 +26,87 @@ export default function AddUserToList() {
     }, [])
 
     function handleChange(e) {
-        const { value, name } = e.target
+        const { value, name} = e.target
         setUser(prevUser => {
             return {
                 ...prevUser,
-                [name]:value,
-                userId: auth?.currentUser?.uid
+                [name]:value
+            }
+        })
+    }
+
+    function handleUpload(file) {
+        setUser(prevUser => {
+            return {
+                ...prevUser,
+                img: file
             }
         })
     }
 
     async function handleSubmit() {
-        try {
-            await addDoc(usersRef), {
-                name: user.name,
-                group: user.group,
-                lawfulVotes: [0],
-                goodVotes: [0],
-
+        if (!user.name || !user.group || !user.img) {
+            alert('please fill all fields')
+        } else {
+            try {
+                const storageRef = ref(storage, `images/${auth?.currentUser?.uid}`);
+                await uploadBytes(storageRef, user.img)
+            } catch(err) {
+                console.log(err)
             }
-        } catch(err) {
-            console.log(err)
+            try {
+                await addDoc(usersRef, {
+                    name: user.name,
+                    group: user.group,
+                    lawfulVotes: [0],
+                    goodVotes: [0],
+                    userId: auth?.currentUser?.uid,
+                    picture: `images/${auth?.currentUser?.uid}`
+                })
+            } catch(err) {
+                console.log(err)
+            }
         }
     }
-
     return(
         <div>
             {/*auth.currentUser.id ?*/
             <div>
-                <input type='text' placeholder='name' value={user.name} name='name' onChange={handleChange}/>
-                <label>Profile Pic: <input type='file' accept="image/png, image/jpeg"/></label>
-                <label htmlFor="group">Select Group:
-                <select name="group" id="group" value={user.group} onChange={handleChange}>
-                    {groups && groups.map(group => {
-                        let groupName = group.groupName
-                        return (
-                            <option value={groupName}>{groupName}</option>
-                        )
-                    })}
-                </select>
+                <input 
+                    type='text' 
+                    placeholder='name' 
+                    value={user.name} 
+                    name='name' 
+                    onChange={handleChange}
+                />
+
+                <label>
+                    Profile Pic: 
+                    <input 
+                        name='img'
+                        onChange={(e) => handleUpload(e.target.files[0])}
+                        className='picupload'
+                        type='file' 
+                        accept="image/png, image/jpeg"
+                    />
+                </label>
+
+                <label htmlFor="group">
+                    Select Group:
+                    <select 
+                        name="group" 
+                        id="group" 
+                        value={user.group} 
+                        onChange={e => handleChange(e)}
+                    >
+                        <option value={null}>Select A Group</option>
+                        {groups && groups.map(group => {
+                            let groupName = group.groupName
+                            return (
+                                <option key={groupName} value={groupName}>{groupName}</option>
+                            )
+                        })}
+                    </select>
                 </label>
                 <button onClick={handleSubmit}>Submit</button>
             </div> 
