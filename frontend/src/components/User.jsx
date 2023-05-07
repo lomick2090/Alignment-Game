a878d202ee32a585960a498d4b24596a620fe3f1
minkman <lomick2090@gmail.com>
2023-05-07 15:49:40 -0500

    download from database

--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -1,6 +1,8 @@
 
 export default function User() {
 
-    return ()
+    return (
+        <div></div>
+    )
     
 }
--- frontend/src/pages/GroupPage.jsx
+++ frontend/src/pages/GroupPage.jsx
@@ -1,8 +1,35 @@
 import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react"
+import { db, auth, storage } from "../config/firebase"
+import { getDocs, collection } from "firebase/firestore"
 
 
 export default function GroupPage() {
+    const [userList, setUserList] = useState([])
+
     const { groupName } = useParams()
+    const usersRef = collection(db, 'users')
+
+    useEffect(() => {
+
+        async function getUserList() {
+            const data = await getDocs(usersRef)
+            const filteredList = data.docs.filter(user => {
+                console.log(user.data().group)
+               return user.data().group == groupName
+            })
+            setUserList(filteredList)
+        }
+
+        getUserList()
+    }, [])
+
+    const userElements = userList.map(user => {
+        const {goodVotes, lawfulVotes, name, picture} = user.data()
+        console.log(goodVotes, lawfulVotes, name, picture)
+    })
+
+    userElements
 
     return (
         <div className="grouppage">
